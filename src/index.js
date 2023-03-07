e01322466a56f0bf0169859e98b6579148f6ce40
Henry Liu <henry.liu@henryli-ltmbm7u.internal.salesforce.com>
2023-03-07 09:51:20 -0800

    added process.env.PORT || to /src/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@
 const app = require('./app');
 const port = app.get('port');
 const name = app.get('name');
-//const server = app.listen(process.env.PORT || port);
-const server = app.listen(port);
+const server = app.listen(process.env.PORT || port);
+//const server = app.listen(port);
 
 server.on('listening', () =>
   console.log(`${name} started at http://${app.get('host')}:${port}`)
